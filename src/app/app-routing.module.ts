99769c5097da11ccebdf2ff36b412dad615f2125
Mahesh Dinkar Pawar <maheshpawar30627@gmail.com>
2024-03-05 11:09:41 +0530

    admin

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RestaurentDashComponent } from './restaurent-dash/restaurent-dash.component';
+import { SignupComponent } from './signup/signup.component';
+import { HelpcComponent } from './helpc/helpc.component';
+import { AdminComponent } from './admin/admin.component';
+
+const routes: Routes = [
+  {
+    path: '', redirectTo: 'login',pathMatch: 'full'
+  },
+  {
+   path: 'login', component: LoginComponent 
+  },
+ {
+   path: 'signup', component: SignupComponent 
+ }, 
+ {
+  path: 'admin', component: AdminComponent
+ },
+ {
+   path:'restaurent' , component: RestaurentDashComponent
+ },
+ {
+  path:'help' , component: HelpcComponent
+}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,74 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { ApiService } from '../shared/api.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+  constructor(private formbuilder: FormBuilder, private _http: HttpClient, private _router: Router,
+    private toastr: ToastrService, private api: ApiService) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.formbuilder.group({
+      email: [''],
+      password: ['']
+    });
+  }
+
+  // SetData()
+  // {
+  //   this.MarvellousForm.setValue(
+  //     {
+  //       username : 'Piyush',
+  //       passowrd : 'abcd',
+  //       ConfirmPass :  'abcd',
+  //       MarvellousClass : 
+  //       {
+  //         batch : 'Python',
+  //         fees : '5000'
+  //       }
+  //     }
+  //   )
+  // }
+
+  // Reset() {
+  //   this.loginForm.reset(
+  //     {
+
+  //     }
+  //   )
+  // }
+
+  logIn() {
+    this.api.useremail = this.loginForm.value.email
+    if(this.api.useremail == "mahesh@gmail.com") {
+      this.api.isAdmin = true
+    }
+      const data = {
+        email: this.loginForm.value.email,
+        password: this.loginForm.value.password
+      }
+
+      this.api.loginAPI(data).subscribe((res: any) => {
+        this.toastr.success(res.msg, "", {
+          timeOut: 2000,
+          closeButton: true
+        });
+        
+        if (res.msg == "Login Successful") {
+          this.loginForm.reset();
+          this._router.navigate(['/restaurent']);
+          let ref = document.getElementById('close');
+          ref?.click();
+        }
+      })
+  }
+}
